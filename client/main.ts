2a076519ef8af29f7bb9a890e5f9bd60e5bf9a01
Olli Aalto <oaalto@gmail.com>
2016-05-05 14:36:50 +0300

    Initial commit

---
+++ client/main.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import { Component } from 'angular2/core';
+import { bootstrap } from 'angular2/platform/browser';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'client/main.html'
+})
+class Socially
+{
+}
+
+bootstrap(Socially);
